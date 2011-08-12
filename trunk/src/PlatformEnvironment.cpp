0b5410656f6fd4ae80e7446cc6216f19f812b2c2
kevin.zqw@gmail.com <kevin.zqw@gmail.com@e1bf4723-dac1-68ea-26c9-3c02dc4ce5e1>
2011-08-12 01:34:49 +0000

    Platform and Environment

---
+++ trunk/src/PlatformEnvironment.cpp
@@ -0,0 +1,60 @@
+#include <Poco/Exception.h>
+#include <Poco/Platform.h>
+#include <Poco/Environment.h>
+#include <gtest/gtest.h>
+
+// Macro: Operating System, Hardware Architecture, Byte Order
+TEST(PlatformEnvironmentTest, Platform)
+{
+#if POCO_OS == POCO_OS_WINDOWS_NT
+	// do the Windows thing
+#elif POCO_OS == POCO_OS_LINUX
+	// do the Linux thing
+#endif
+
+#if defined(POCO_OS_FAMILY_WINDOWS)
+	// do the Windows thing
+#elif defined(POCO_OS_FAMILY_UNIX)
+	// do the Unix thing
+#endif
+
+#if POCO_ARCH == POCO_ARCH_IA32
+	// do the Intel 32-bit thing
+#elif POCO_ARCH == ARM
+	// do the ARM thing
+#endif
+
+#ifdef POCO_ARCH_LITTLE_ENDIAN
+	// little endian
+#elif POCO_ARCH_BIG_ENDIAN
+	// big endian
+#endif
+
+	SUCCEED();
+}
+
+TEST(PlatformEnvironmentTest, Environment)
+{
+	EXPECT_NO_THROW({
+		std::string path = Poco::Environment::get("path");
+		EXPECT_TRUE(path.length() != 0);
+	});
+
+	EXPECT_THROW({
+		Poco::Environment::get("NotExpectedExistVariable");
+	}, Poco::NotFoundException);
+
+	EXPECT_FALSE(Poco::Environment::has("NotExpectedExistVariable"));
+
+	std::string setValue = "justTest";
+	Poco::Environment::set("MustExistVariable", setValue);
+	EXPECT_NO_THROW({
+		EXPECT_EQ(setValue, Poco::Environment::get("MustExistVariable"));
+	});
+	
+	EXPECT_FALSE(Poco::Environment::osName().empty());
+	EXPECT_FALSE(Poco::Environment::osVersion().empty());
+	EXPECT_FALSE(Poco::Environment::osArchitecture().empty());
+	EXPECT_FALSE(Poco::Environment::nodeName().empty());
+	EXPECT_FALSE(Poco::Environment::nodeId().empty());
+}
